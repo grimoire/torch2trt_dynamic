fdf2afaaec131189bbce47c139aae9c05484babf
grimoire <streetyao@live.com>
2020-04-01 21:33:11 +0800

    keep secret

--- plugins/globals.py
+++ plugins/globals.py
@@ -1,4 +1,3 @@
 import os
 import os.path as osp
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-dir_path = "/home/yq/space/trt_plugin/build/lib"
+dir_path = os.path.dirname(os.path.realpath(__file__))
