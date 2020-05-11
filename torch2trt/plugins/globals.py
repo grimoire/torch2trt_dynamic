881f829b7e24cff1b875259f4d47fbddf428eafb
grimoire <streetyao@live.com>
2020-05-11 13:50:16 +0800

    custom update

--- torch2trt/plugins/globals.py
+++ torch2trt/plugins/globals.py
@@ -1,3 +1,5 @@
 import os
 import os.path as osp
-dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path = osp.join( os.path.expanduser('~'), "space/trt_plugin/build/lib/")
+if not osp.exists(dir_path):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
