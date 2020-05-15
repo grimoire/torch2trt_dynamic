547a648c2c6644cd6e2fc540e15aa619b2356d60
grimoire <streetyao@live.com>
2020-05-15 12:45:31 +0800

    add negative shape back

--- torch2trt/converters/view.py
+++ torch2trt/converters/view.py
@@ -25,38 +25,23 @@ def convert_view(ctx):
             break
 
     ## negative shape might cause overflow, forbid for now
-    for s in size:
-        if s<0:
-            is_shape_tensor=False
-            break
+    # for s in size:
+    #     if s<0:
+    #         is_shape_tensor=False
+    #         break
 
     ## compute shape tensor
     if support_dynamic_shape and is_shape_tensor:
         shape_trt = []
-        minus_index=-1
         for idx, s in enumerate(size):
             if isinstance(s, IntWarper):
                 shape_trt.append(s._trt)
             else:
-                if s>0:
-                    const_shape_trt = trt_(ctx.network, input.new_tensor([s],dtype=torch.int32))
-                    shape_trt.append(const_shape_trt)
-                else:
-                    minus_index = idx
-                    shape_trt.append(-1)
+                if s<0:
+                    print("warning: negative index of view/reshape might cause overflow!")
+                const_shape_trt = trt_(ctx.network, input.new_tensor([s],dtype=torch.int32))
+                shape_trt.append(const_shape_trt)
 
-        if minus_index>=0:
-            input_shape_trt = ctx.network.add_shape(input_trt).get_output(0)
-            
-            # prod reduce of input shape
-            input_reduce_trt = ctx.network.add_reduce(input_shape_trt, trt.ReduceOperation.PROD, axes=1, keep_dims=True).get_output(0)
-            # prod of positive output shape
-            positive_shape_trt = shape_trt[:minus_index] + shape_trt[minus_index+1:]
-            output_reduce_trt = positive_shape_trt[0]
-            for next_shape_trt in positive_shape_trt[1:]:
-                output_reduce_trt = ctx.network.add_elementwise(output_reduce_trt, next_shape_trt, trt.ElementWiseOperation.PROD).get_output(0)
-            negative_shape_trt = ctx.network.add_elementwise(input_reduce_trt, output_reduce_trt, trt.ElementWiseOperation.FLOOR_DIV).get_output(0)
-            shape_trt[minus_index] = negative_shape_trt
         shape_trt = ctx.network.add_concatenation(shape_trt).get_output(0)
 
     layer = ctx.network.add_shuffle(input_trt)
