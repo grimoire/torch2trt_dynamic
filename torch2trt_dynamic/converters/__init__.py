ce4598e7461d9845ec3c36ca085e5f16ae763aa8
grimoire <streetyao@live.com>
2020-10-23 17:45:11 +0800

    add leaky_relu support

--- torch2trt_dynamic/converters/__init__.py
+++ torch2trt_dynamic/converters/__init__.py
@@ -89,6 +89,7 @@ from .full import convert_full
 from .reciprocal import convert_reciprocal
 from .take import convert_take
 from .where import convert_where, convert_Tensor_where
+from .leaky_relu import convert_leaky_relu
 
 try:
     # custom plugin support
