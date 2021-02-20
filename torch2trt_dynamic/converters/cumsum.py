26267aaf0f372e804dd5f1821528c757f1aee442
grimoire <streetyao@live.com>
2021-02-20 16:34:19 +0800

    better cumsum

--- torch2trt_dynamic/converters/cumsum.py
+++ torch2trt_dynamic/converters/cumsum.py
@@ -1,26 +1,51 @@
 from ..torch2trt_dynamic import *
+from .cast_type import convert_type
 
 from ..plugins import *
+
 
 @tensorrt_converter('torch.cumsum')
 @tensorrt_converter('torch.Tensor.cumsum')
 def convert_cumsum(ctx):
+    old_args = ctx.method_args
+    old_kwargs = ctx.method_kwargs
     input = ctx.method_args[0]
     dim = get_arg(ctx, 'dim', pos=1, default=0)
     cum_type = 0
 
-    if dim<0:
-        dim = len(input.shape)+dim
-    input_trt = trt_(ctx.network, input)
+    if dim < 0:
+        dim = len(input.shape) + dim
     output = ctx.method_return
 
+    if input.dtype == torch.bool or input.dtype == bool:
+        cast_input = input.type_as(output)
+        ctx.method_args = [input]
+        ctx.method_kwargs = {}
+        ctx.method_return = cast_input
+        convert_type(ctx, torch_dtype_to_trt(output.dtype))
+        input_trt = trt_(ctx.network, cast_input)
+    else:
+        input_trt = trt_(ctx.network, input)
+
     plugin = create_torchcum_plugin("cumsum_" + str(id(input)),
-                                            dim=dim,
-                                            cum_type=cum_type
-                                            )
-    
-    custom_layer = ctx.network.add_plugin_v2(
-        inputs=[input_trt], plugin=plugin)
+                                    dim=dim,
+                                    cum_type=cum_type)
 
-    output._trt = custom_layer.get_output(0)
+    custom_layer = ctx.network.add_plugin_v2(inputs=[input_trt], plugin=plugin)
 
+    output_trt = custom_layer.get_output(0)
+
+    if input.dtype != output.dtype:
+        tmp_output = output.clone()
+        tmp_output._trt = output_trt
+        ctx.method_args = [tmp_output]
+        ctx.method_kwargs = {}
+        ctx.method_return = output
+        convert_type(ctx, torch_dtype_to_trt(output.dtype))
+        output_trt = ctx.method_return._trt
+
+    output._trt = output_trt
+
+    ctx.method_args = old_args
+    ctx.method_kwargs = old_kwargs
+    ctx.method_return = output
